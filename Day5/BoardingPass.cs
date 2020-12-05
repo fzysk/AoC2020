2d24e301dfc53338d9be393b0f4808d41fd567f9
Filip Zyskowski <filipzyskowski@gmail.com>
2020-12-05 14:07:57 +0100

    day5 - part 1

---
+++ Day5/BoardingPass.cs
@@ -0,0 +1,21 @@
+﻿namespace Day5
+{
+    internal class BoardingPass
+    {
+        private const int ID_MULTIPLIER = 8;
+
+        public int Row { get; private set; }
+        public int Column { get; private set; }
+
+        public BoardingPass(int row, int column)
+        {
+            Row = row;
+            Column = column;
+        }
+
+        public int GetSeatID()
+        {
+            return Row * ID_MULTIPLIER + Column;
+        }
+    }
+}
---
+++ Day5/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Day5
+{
+    class Program
+    {
+        private static readonly Regex passRegex = new Regex("([FB]{7})([RL]{3})");
+
+        private const char ROWS_BINARY_ONE = 'B';
+        private const char COLUMNS_BINARY_ONE = 'R';
+        private const int BINARY_BASE = 2;
+
+        static void Main(string[] args)
+        {
+            string[] input = File.ReadAllLines("input.txt");
+
+            int maxId = input.Select(line => LineToBoardingPass(line)).Max(pass => pass.GetSeatID());
+
+            Console.WriteLine(maxId);
+        }
+
+        static BoardingPass LineToBoardingPass(string line)
+        {
+            Match match = passRegex.Match(line);
+
+            int row = GetRow(match.Groups[1].Value);
+            int column = GetColumn(match.Groups[2].Value);
+
+            return new BoardingPass(row, column);
+        }
+
+        private static int GetRow(string value)
+        {
+            return CalculateBinaryValue(value, ROWS_BINARY_ONE);
+        }
+
+        private static int GetColumn(string value)
+        {
+            return CalculateBinaryValue(value, COLUMNS_BINARY_ONE);
+        }
+
+        private static int CalculateBinaryValue(string value, char binaryOne)
+        {
+            int result = 0;
+            for (int i = value.Length - 1, j = 0; i >= 0; i--, j++)
+            {
+                if (value[i] == binaryOne)
+                {
+                    result += (int)Math.Pow(BINARY_BASE, j);
+                }
+            }
+
+            return result;
+        }
+    }
+}
