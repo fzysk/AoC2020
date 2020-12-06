0851c0b05895c816e19d442f129e17c85262202f
Filip Zyskowski <filipzyskowski@gmail.com>
2020-12-06 17:22:08 +0100

    day6 - part 1

---
+++ Day6/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Day6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = File.ReadAllLines("input.txt");
+
+            int yesCount = 0;
+            foreach (IEnumerable<string> answerGroup in GetGroupedAnswers(input))
+            {
+                var answerDict = new Dictionary<char, int>();
+                foreach (string answers in answerGroup)
+                {
+                    foreach (char answer in answers)
+                    {
+                        if (!answerDict.ContainsKey(answer))
+                        {
+                            answerDict.Add(answer, 0);
+                        }
+                    }
+                }
+
+                yesCount += answerDict.Keys.Count;
+            }
+
+            Console.WriteLine(yesCount);
+        }
+
+        private static IEnumerable<IEnumerable<string>> GetGroupedAnswers(string[] input)
+        {
+            int prevBreak = 0;
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] == "")
+                {
+                    yield return input.Skip(prevBreak).Take(i - prevBreak);
+                    prevBreak = i + 1;
+                }
+            }
+
+            yield return input.Skip(prevBreak);
+        }
+    }
+}
