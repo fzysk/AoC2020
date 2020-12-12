dbae572228f33a0461bd774792e0879e310668dd
Filip Zyskowski <filipzyskowski@gmail.com>
2020-12-12 23:44:57 +0100

    day10 - part 2

--- Day10/Program.cs
+++ Day10/Program.cs
@@ -12,7 +12,12 @@ namespace Day10
 
             // concat start and end values
             adapters = new int[] { 0 }.Concat(adapters).Concat(new int[] { adapters[adapters.Length - 1] + 3 }).ToArray();
+            Part1(adapters);
+            Part2(adapters);
+        }
 
+        private static void Part1(int[] adapters)
+        {
             int oneStepCounter = 0, threeStepCounter = 0;
             for (int i = 0; i < adapters.Length - 1; i++)
             {
@@ -34,6 +39,35 @@ namespace Day10
             }
 
             Console.WriteLine(oneStepCounter * threeStepCounter);
+        }
+
+        private static void Part2(int[] adapters)
+        {
+            int maxDiff = 3;
+            long[] solutions = new long[adapters.Length];
+
+            // start-off
+            solutions[1] = 1;
+            solutions[2] = 2;
+            solutions[3] = 4;
+            
+            for (int i = 4; i < adapters.Length; i++)
+            {
+                // previous is always reachable
+                solutions[i] += solutions[i - 1];
+
+                if (adapters[i] - adapters[i - 2] <= maxDiff)
+                {
+                    solutions[i] += solutions[i - 2];
+                }
+
+                if (adapters[i] - adapters[i - 3] <= maxDiff)
+                {
+                    solutions[i] += solutions[i - 3];
+                }
+            }
+
+            Console.WriteLine(solutions[solutions.Length - 2]);
         }
     }
 }
