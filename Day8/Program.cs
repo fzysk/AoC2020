7974875d409f117c970df88b69db4cfafd91c171
Filip Zyskowski <filipzyskowski@gmail.com>
2020-12-12 19:35:24 +0100

    day8 - part 2

--- Day8/Program.cs
+++ Day8/Program.cs
@@ -9,43 +9,90 @@ namespace Day8
         static void Main(string[] args)
         {
             string[] data = File.ReadAllLines("input.txt");
-            var operationsDone = new Dictionary<int, char>();
 
-            int i = 0, accumulate = 0;
-
-            while (i < data.Length)
+            foreach (string[] generatedData in GenerateFixUpData(data))
             {
-                // check break condition
-                if (operationsDone.ContainsKey(i))
+                bool success = true;
+                int i = 0, accumulate = 0;
+                var operationsDone = new Dictionary<int, string>();
+
+                while (i < generatedData.Length)
                 {
-                    break;
+                    // check break condition
+                    if (operationsDone.ContainsKey(i))
+                    {
+                        success = false;
+                        break;
+                    }
+
+                    // add line as processed
+                    operationsDone.Add(i, generatedData[i]);
+
+                    // process line
+                    (Operations op, int modifier) = ProcessLine(generatedData[i]);
+
+                    switch (op)
+                    {
+                        case Operations.nop:
+                            i++;
+                            break;
+                        case Operations.acc:
+                            accumulate += modifier;
+                            i++;
+                            break;
+                        case Operations.jmp:
+                            i += modifier;
+                            break;
+                        default:
+                            throw new InvalidOperationException("Operation not handled");
+                    }
                 }
 
-                // add line as processed
-                operationsDone.Add(i, '\0');
-
-                // process line
-                string op = data[i].Substring(0, 3);
-                int modifier = int.Parse(data[i].Substring(4, data[i].Length - 4));
-
-                switch ((Operations)Enum.Parse(typeof(Operations), op))
+                if (success)
                 {
-                    case Operations.nop:
-                        i++;
-                        break;
-                    case Operations.acc:
-                        accumulate += modifier;
-                        i++;
-                        break;
-                    case Operations.jmp:
-                        i += modifier;
-                        break;
-                    default:
-                        throw new InvalidOperationException("Operation not handled");
+                    Console.WriteLine(accumulate);
+                    return;
+                }
+            }
+        }
+
+        private static (Operations op, int modifier) ProcessLine(string line)
+        {
+            Operations op = (Operations)Enum.Parse(typeof(Operations), line.Substring(0, 3));
+            int modifier = int.Parse(line.Substring(4, line.Length - 4));
+
+            return (op, modifier);
+        }
+
+        static IEnumerable<string[]> GenerateFixUpData(string[] originalData)
+        {
+            for (int i = 0; i < originalData.Length; i++)
+            {
+                (Operations op, int modifier) = ProcessLine(originalData[i]);
+
+                if (op == Operations.jmp)
+                {
+                    originalData[i] = originalData[i].Replace("jmp", "nop");
+                    
+                    // let's try this data
+                    yield return originalData;
+
+                    // rollback
+                    originalData[i] = originalData[i].Replace("nop", "jmp");
+                }
+                else if (op == Operations.nop && !(modifier == 0 || modifier == 1))
+                {
+                    originalData[i] = originalData[i].Replace("nop", "jmp");
+
+                    // let's try this one
+                    yield return originalData;
+
+                    // rollback
+                    originalData[i] = originalData[i].Replace("jmp", "nop");
                 }
             }
 
-            Console.WriteLine(accumulate);
+            throw new Exception("Got to the end, changed everything");
         }
     }
 }
